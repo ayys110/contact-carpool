b991ce19d4419e303378fdd54441737d2e1e25eb
venkateshkyama <venkateshkyama@gmail.com>
2012-04-19 04:50:01 +0000

    CarPool

---
+++ CarPool/src/test/java/net/kv/carpool/controller/BaseControllerTest.java
@@ -0,0 +1,41 @@
+/**
+ * BaseControllerTest.java is used for 
+ * 
+ * @author chaitanya
+ * Created on Apr 17, 2012
+ */
+package net.kv.carpool.controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+/**
+ * @author chaitanya
+ *
+ */
+//@ContextConfiguration
+//@RunWith(SpringJUnit4)
+public class BaseControllerTest implements ApplicationContextAware{
+
+	GenericXmlApplicationContext xmlBean = new GenericXmlApplicationContext("/WEB-INF/cp-servlet.xml");
+	
+	private BaseController baseController;
+	@Test
+	public void test() {
+		assertEquals("/index", baseController.onGet());
+	}
+
+	public void setApplicationContext(ApplicationContext applicationContext)
+			throws BeansException {
+		//baseController
+		baseController = (BaseController) applicationContext.getBean("baseController");
+	}
+
+}
