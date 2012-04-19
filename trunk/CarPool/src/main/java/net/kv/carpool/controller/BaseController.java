362dc9c807c8006412d1e723721308aa06fe42cf
venkateshkyama@gmail.com <venkateshkyama@gmail.com@60a74269-60bf-4380-39d4-65d6af817004>
2012-04-19 04:50:01 +0000

    CarPool

---
+++ trunk/CarPool/src/main/java/net/kv/carpool/controller/BaseController.java
@@ -0,0 +1,26 @@
+/**
+ * BaseController.java is used for 
+ * 
+ * @author chaitanya
+ * Created on Apr 6, 2012
+ */
+package net.kv.carpool.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @author chaitanya
+ * 
+ */
+@Controller
+@RequestMapping(value = "/where")
+public class BaseController {
+
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String onGet() {
+		return "index";
+	}
+}
---
+++ trunk/CarPool/src/main/webapp/WEB-INF/pages/404.jsp
@@ -0,0 +1,3 @@
+<p>Page is in the Restroom. Please come back later.</p>
+&nbsp;
+<a href="index.htm">Go to index page</a>
