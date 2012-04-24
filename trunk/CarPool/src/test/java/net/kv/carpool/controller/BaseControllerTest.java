be8d15d9547a2d6c85228e93d3435d703d58505c
venkateshkyama@gmail.com <venkateshkyama@gmail.com@60a74269-60bf-4380-39d4-65d6af817004>
2012-04-24 03:11:01 +0000

    formated code

--- trunk/CarPool/src/test/java/net/kv/carpool/controller/BaseControllerTest.java
+++ trunk/CarPool/src/test/java/net/kv/carpool/controller/BaseControllerTest.java
@@ -1,41 +1,41 @@
 /**
- * BaseControllerTest.java is used for 
+ * BaseControllerTest.java is used for
  * 
- * @author chaitanya
- * Created on Apr 17, 2012
+ * @author chaitanya Created on Apr 17, 2012
  */
 package net.kv.carpool.controller;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
 /**
  * @author chaitanya
- *
+ * 
  */
 //@ContextConfiguration
 //@RunWith(SpringJUnit4)
-public class BaseControllerTest implements ApplicationContextAware{
+public class BaseControllerTest implements ApplicationContextAware
+{
 
-	GenericXmlApplicationContext xmlBean = new GenericXmlApplicationContext("/WEB-INF/cp-servlet.xml");
-	
-	private BaseController baseController;
-	@Test
-	public void test() {
-		assertEquals("/index", baseController.onGet());
-	}
+    GenericXmlApplicationContext xmlBean = new GenericXmlApplicationContext("/WEB-INF/cp-servlet.xml");
 
-	public void setApplicationContext(ApplicationContext applicationContext)
-			throws BeansException {
-		//baseController
-		baseController = (BaseController) applicationContext.getBean("baseController");
-	}
+    private BaseController baseController;
+
+    @Test
+    public void test()
+    {
+        assertEquals("/index", baseController.onGet());
+    }
+
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException
+    {
+        // baseController
+        baseController = (BaseController) applicationContext.getBean("baseController");
+    }
 
 }
